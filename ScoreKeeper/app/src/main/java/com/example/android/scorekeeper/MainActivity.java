d2c9459d7a6c9eaf3ae0740738c129d3b1632a11
matthew-t-smith <matthew.t.smith@hotmail.com>
2018-04-11 11:22:47 +1200

    Finished Score Keeper app
    Tennis scoring app (basic scoring) completed!

--- ScoreKeeper/app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ ScoreKeeper/app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -3,142 +3,120 @@ package com.example.android.scorekeeper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.android.scorekeeper.R;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     int aGame = 0;
     int bGame = 0;
-    int aSet1 = 0;
-    int aSet2 = 0;
-    int aSet3 = 0;
-    int bSet1 = 0;
-    int bSet2 = 0;
-    int bSet3 = 0;
+    int aSet = 0;
+    int bSet = 0;
     int aMatch = 0;
     int bMatch = 0;
-    boolean aGameWin = false; /** 60, by 2 */
-    boolean aSetWin = false; /** 6 min, by 2 */
-    boolean aMatchWin = false; /** best 2 of 3 */
-    boolean deuce = false; /** game tied at 40 */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForA(aGame);
-        displayForB(bGame);
+        displayForA();
+        displayForB();
     }
 
     public void score(View view) {
         switch(view.getId()) {
             case R.id.a_point:
-                switch(aGame) {
-                    case 0:
-                    case 15:
-                        aGame = aGame + 15;
-                    break;
-
-                    case 30:
-                        aGame = aGame + 10;
-                    break;
-
-                    case 40:
-                        if(isDeuce(aGame, bGame)) {
-                            aGame = aGame + 10;
-                            break;
-                        }
-                        aGameWin = true;
-                        gameWin(aGameWin);
+                aGame += 1;
+                if(gameWin(aGame, bGame)) {
+                    aGame = 0;
+                    bGame = 0;
+                    aSet = aSet + 1;
                 }
-                break;
+                if(setWin(aSet, bSet)) {
+                    aSet = 0;
+                    bSet = 0;
+                    aMatch = aMatch + 1;
+                }
+                if(win(aMatch)) {
+                    EditText e = (EditText)findViewById(R.id.player_a);
+                    String aName = e.getText().toString();
+                    displayWin(aName);
+                }
+            break;
 
             case R.id.b_point:
-                switch(bGame) {
-                    case 0:
-                    case 15:
-                        bGame = bGame + 15;
-                        break;
-
-                    case 30:
-                        bGame = bGame + 10;
-                        break;
-
-                    case 40:
-                        if(isDeuce(bGame, bGame)) {
-                            bGame = bGame + 10;
-                            break;
-                        }
-                        gameWin(aGameWin);
+                bGame += 1;
+                if(gameWin(aGame, bGame)) {
+                    aGame = 0;
+                    bGame = 0;
+                    bSet = bSet + 1;
                 }
-                break;
+                if(setWin(aSet, bSet)) {
+                    aSet = 0;
+                    bSet = 0;
+                    bMatch = bMatch + 1;
+                }
+                if(win(bMatch)) {
+                    EditText e = (EditText)findViewById(R.id.player_b);
+                    String bName = e.getText().toString();
+                    displayWin(bName);
+                }
+            break;
 
         };
-        displayForA(aGame);
-        displayForB(bGame);
+        displayForA();
+        displayForB();
     }
 
-    public boolean isDeuce(int a, int b) {
-        if(a == b && a == 40) {
-            return true;
-        } else {
-            return false;
-        }
+    public boolean gameWin(int a, int b) {
+        return (((a-b) > 1 && a > 3) || ((b-a) > 1 && b > 3));
     }
 
-    public void gameWin(boolean a) {
-        aGame = 0;
-        bGame = 0;
-        if(a) {
-            aSet = aSet + 1;
-            aGameWin = false;
-        } else {
-            bSet = bSet + 1;
-        }
+    public boolean setWin(int a, int b) {
+        return (((a-b) > 1 && a > 5) || ((b-a) > 1 && b > 5));
     }
 
-    public void setWin(boolean a) {
-
+    public boolean win(int s) {
+        return (s == 2);
     }
 
-    public void matchWin(boolean a) {
-
+    public void displayForA() {
+        TextView gameView = (TextView) findViewById(R.id.a_game_score);
+        TextView setView = (TextView) findViewById(R.id.a_set_score);
+        TextView matchView = (TextView) findViewById(R.id.a_match_score);
+        gameView.setText(String.valueOf(aGame));
+        setView.setText(String.valueOf(aSet));
+        matchView.setText(String.valueOf(aMatch));
     }
 
-    /**
-     * Displays the given score for Team A.
-     */
-    public void displayForA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.a_point);
-        scoreView.setText(String.valueOf(score));
+    public void displayForB() {
+        TextView gameView = (TextView) findViewById(R.id.b_game_score);
+        TextView setView = (TextView) findViewById(R.id.b_set_score);
+        TextView matchView = (TextView) findViewById(R.id.b_match_score);
+        gameView.setText(String.valueOf(bGame));
+        setView.setText(String.valueOf(bSet));
+        matchView.setText(String.valueOf(bMatch));
     }
 
-    /**
-     * Displays the given score for Team B.
-     */
-    public void displayForB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.b_point);
-        scoreView.setText(String.valueOf(score));
+    public void displayWin(String name) {
+        TextView winnerView = (TextView) findViewById(R.id.winner);
+        winnerView.setText(String.valueOf(name + " Wins!"));
     }
 
     public void resetDisplay(View view) {
-        int aGame = 0;
-        int bGame = 0;
-        int aSet1 = 0;
-        int aSet2 = 0;
-        int aSet3 = 0;
-        int bSet1 = 0;
-        int bSet2 = 0;
-        int bSet3 = 0;
-        int aMatch = 0;
-        int bMatch = 0;
-        boolean aGameWin = false;
-        boolean aSetWin = false;
-        boolean aMatchWin = false;
-        boolean deuce = false;
-        displayForA(aGame);
-        displayForB(bGame);
+        aGame = 0;
+        bGame = 0;
+        aSet = 0;
+        bSet = 0;
+        aMatch = 0;
+        bMatch = 0;
+        displayForA();
+        displayForB();
+        TextView winnerView = (TextView) findViewById(R.id.winner);
+        winnerView.setText(String.valueOf(""));
     }
 }
