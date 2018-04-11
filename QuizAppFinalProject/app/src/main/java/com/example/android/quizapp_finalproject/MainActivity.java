219992f30dac75d02c56befdc5c6ddb4edd1092b
matthew-t-smith <matthew.t.smith@hotmail.com>
2018-04-11 18:52:31 +1200

    Fixed issue in scoring

--- QuizAppFinalProject/app/src/main/java/com/example/android/quizapp_finalproject/MainActivity.java
+++ QuizAppFinalProject/app/src/main/java/com/example/android/quizapp_finalproject/MainActivity.java
@@ -35,71 +35,57 @@ public class MainActivity extends AppCompatActivity {
         RadioGroup first = (RadioGroup) findViewById(R.id.question_1);
         int one = first.getCheckedRadioButtonId();
         RadioButton name_1 = (RadioButton) findViewById(one);
-        String question_one = name_1.getText().toString();
 
         RadioGroup second = (RadioGroup) findViewById(R.id.question_2);
         int two = second.getCheckedRadioButtonId();
         RadioButton name_2 = (RadioButton) findViewById(two);
-        String question_two = name_2.getText().toString();
 
         RadioGroup third = (RadioGroup) findViewById(R.id.question_3);
         int three = third.getCheckedRadioButtonId();
         RadioButton name_3 = (RadioButton) findViewById(three);
-        String question_three = name_3.getText().toString();
 
         RadioGroup fourth = (RadioGroup) findViewById(R.id.question_4);
         int four = fourth.getCheckedRadioButtonId();
         RadioButton name_4 = (RadioButton) findViewById(four);
-        String question_four = name_4.getText().toString();
 
         RadioGroup fifth = (RadioGroup) findViewById(R.id.question_5);
         int five = fifth.getCheckedRadioButtonId();
         RadioButton name_5 = (RadioButton) findViewById(five);
-        String question_five = name_5.getText().toString();
 
         RadioGroup sixth = (RadioGroup) findViewById(R.id.question_6);
         int six = sixth.getCheckedRadioButtonId();
         RadioButton name_6 = (RadioButton) findViewById(six);
-        String question_six = name_6.getText().toString();
 
         RadioGroup seventh = (RadioGroup) findViewById(R.id.question_7);
         int seven = seventh.getCheckedRadioButtonId();
         RadioButton name_7 = (RadioButton) findViewById(seven);
-        String question_seven = name_7.getText().toString();
 
         RadioGroup eighth = (RadioGroup) findViewById(R.id.question_8);
         int eight = eighth.getCheckedRadioButtonId();
         RadioButton name_8 = (RadioButton) findViewById(eight);
-        String question_eight = name_8.getText().toString();
 
-//   Show answers submitted:
-//        String answers = question_one + "\n" + question_two + "\n" + question_three + "\n" +
-//                question_four + "\n" + question_five + "\n" + question_six + "\n" + question_seven + "\n"
-//                + question_eight + "\n";
-//        Toast.makeText(getApplicationContext(), answers, Toast.LENGTH_LONG).show();
-
-        if(question_one == "n-1") {
+        if(name_1.getId() == R.id.d_1) {
             score += 1;
         }
-        if(question_two == "Segment tree") {
+        if(name_2.getId() == R.id.b_2) {
             score += 1;
         }
-        if(question_three == "Stack") {
+        if(name_3.getId() == R.id.b_3) {
             score += 1;
         }
-        if(question_four == "client") {
+        if(name_4.getId() == R.id.a_4) {
             score += 1;
         }
-        if(question_five == "0") {
+        if(name_5.getId() == R.id.c_5) {
             score += 1;
         }
-        if(question_six == "final") {
+        if(name_6.getId() == R.id.a_6) {
             score += 1;
         }
-        if(question_seven == "int 1_var;") {
+        if(name_7.getId() == R.id.d_7) {
             score += 1;
         }
-        if(question_eight == "Extending") {
+        if(name_8.getId() == R.id.b_8) {
             score += 1;
         }
 
